ef45c1ea4cd00fd4b9a0aa913b54df885f14fadc
kyle.chang <kyle.chang.cfk@gmail.com>
2025-12-05 01:48:10 +0800

    refactor: remove MainLayoutProps and ue BaseChildren instead

--- src/shared/layouts/main-layout/MainLayout.tsx
+++ src/shared/layouts/main-layout/MainLayout.tsx
@@ -1,18 +1,13 @@
 import { useLocation } from 'react-router-dom';
 
-import type { ReactNode } from 'react';
+import type { BaseChildren } from '../../../core/interfaces/base-children.interface';
 
-interface MainLayoutProps {
-	children: ReactNode;
-}
-
-const MainLayout = ({ children }: MainLayoutProps) => {
+const MainLayout = ({ children }: BaseChildren) => {
 	const location = useLocation();
 	const isDashboard = location.pathname === '/dashboard';
 
 	return (
 		<div className="min-h-screen bg-slate-950 selection:bg-primary-500/30 font-sans">
-			<header>Header</header>
 			<main>{children}</main>
 			{!isDashboard && <footer>Footer</footer>}
 		</div>
