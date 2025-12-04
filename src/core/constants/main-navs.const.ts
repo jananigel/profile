2793a35a4008fdde9284e2c799194a1914ddcec0
kyle.chang <kyle.chang.cfk@gmail.com>
2025-12-05 00:45:39 +0800

    feat: create the main navs constant

---
+++ src/core/constants/main-navs.const.ts
@@ -0,0 +1,6 @@
+export const MAIN_NAVS = [
+	{ name: 'About', href: '#hero' },
+	{ name: 'Experience', href: '#experience' },
+	{ name: 'Skills', href: '#skills' },
+	{ name: 'Projects', href: '#projects' },
+];
