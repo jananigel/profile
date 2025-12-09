43eb0b770548cf606c09ba4c429b830414738af2
kyle <kyle.chang@ackuretta.com>
2025-12-09 19:05:22 +0800

    feat: create the skill section component and update the skill card component

--- src/shared/components/skills/skill-card/skill-card.tsx
+++ src/shared/components/skills/skill-card/skill-card.tsx
@@ -1,31 +1,39 @@
 import { motion } from 'framer-motion';
-import { Mail } from 'lucide-react';
 
-const SkillCard = () => {
-	const skill = {
-		name: 'test',
-		highlight: true,
+import { CATEGORY_ICONS } from '../../../../core/constants/skill-category-icon.const';
+
+import type { Skill } from '../../../../core/interfaces/skill.interface';
+
+type Skills = { skills: Skill[] };
+
+const SkillCard = ({ skills }: Skills) => {
+	const Icon = CATEGORY_ICONS[skills[0].category];
+
+	const item = {
+		hidden: { opacity: 0, y: 20 },
+		show: { opacity: 1, y: 0 },
 	};
-
-	const category = 'frontend';
 
 	return (
 		<>
 			<motion.div
+				variants={item}
 				whileHover={{ y: -5, borderColor: 'rgba(14, 165, 233, 0.5)' }}
 				className="bg-slate-900/50 rounded-xl p-6 border border-slate-800 transition-colors duration-300">
 				<div className="flex items-center gap-3 mb-6">
 					<div className="p-2 bg-slate-800 rounded-lg text-primary-400">
-						<Mail size={24} />
+						<Icon size={24} />
 					</div>
-					<h3 className="text-lg font-bold text-white">{category}</h3>
+					<h3 className="text-lg font-bold text-white">{skills[0].category}</h3>
 				</div>
 
 				<div className="flex flex-wrap gap-2">
-					<motion.div
-						key={'skill.name'}
-						whileHover={{ scale: 1.05 }}
-						className={`
+					{skills.map((skill: Skill) => {
+						return (
+							<motion.div
+								key={skill.name}
+								whileHover={{ scale: 1.05 }}
+								className={`
                         px-3 py-1.5 rounded-md text-sm border transition-all cursor-default
                         ${
 													skill.highlight
@@ -33,8 +41,10 @@ const SkillCard = () => {
 														: 'bg-slate-800/50 border-slate-700 text-slate-300'
 												}
                       `}>
-						{skill.name}
-					</motion.div>
+								{skill.name}
+							</motion.div>
+						);
+					})}
 				</div>
 			</motion.div>
 		</>
