9f5874458aa95475f9bbbc30c9b686fa0214eaf9
kyle <kyle.chang@ackuretta.com>
2025-12-10 11:14:08 +0800

    feat: create the constant for the project data

---
+++ src/core/constants/projects.const.tsx
@@ -0,0 +1,25 @@
+import type { Project } from '../interfaces';
+
+export const PROJECTS: Project[] = [
+	{
+		title: 'Angular - Video Player',
+		description:
+			'Integrate Youtube API, Organize the project structure for scalability and maintainability.',
+		techStack: ['Angular', 'Youtube API'],
+		type: 'Github',
+	},
+	{
+		title: 'Angular - Resizable Layout',
+		description:
+			'Create a directive to enable the Layout to be resizable by the user, enhancing UI flexibility.',
+		techStack: ['Angular', 'Directives', 'DOM Manipulation'],
+		type: 'Github',
+	},
+	{
+		title: 'React - Login Releate',
+		description:
+			'A comprehensive login system with form validation, password generator, toast component, and modal component.',
+		techStack: ['React', 'Forms', 'UI Components'],
+		type: 'Github',
+	},
+];
