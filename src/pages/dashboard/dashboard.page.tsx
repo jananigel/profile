b17117be8f5a1c671445e995831be9710049f9ab
kyle <kyle.chang@ackuretta.com>
2025-12-11 10:42:32 +0800

    feat: add the dashboard title component to the dashboard page

--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -1,9 +1,11 @@
+import { DashboardTitle } from '../../shared/components/dashboard/';
+
 const DashboardPage = () => {
 	return (
 		<>
 			<div className="pt-24 pb-12 px-4 min-h-screen bg-slate-950 text-slate-200 font-sans">
 				<div className="max-w-7xl mx-auto space-y-6">
-					<p className="text-white">Dashboard page</p>
+					<DashboardTitle></DashboardTitle>
 				</div>
 			</div>
 		</>
