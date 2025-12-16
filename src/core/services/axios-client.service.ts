9f5dd199565561fe67f04108b71087a28cba6e1a
kyle.chang <kyle.chang.cfk@gmail.com>
2025-12-16 23:26:10 +0800

    feat: add content type to the axios client

--- src/core/services/axios-client.service.ts
+++ src/core/services/axios-client.service.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosClient = axios.create({
 	baseURL: import.meta.env.BASE_URL,
 	timeout: 10000,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 });
 
 import { HTTP_ERROR_HANDLER_INTERCEPTOR } from './interceptors/http-error-handler.interceptor';
